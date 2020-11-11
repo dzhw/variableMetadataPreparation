b4e380b876bd0f4e8b6c52b4dc4eaa9ff0b63d9f
Robert Birkelbach <birkelbach@dzhw.eu>
2020-11-11 15:22:00 +0100

    Implement copying of results and option parsing (dzhw/metadatamanagement#2685)

---
+++ R/utils-copy_results.R
@@ -0,0 +1,51 @@
+options(repos =
+    c(CRAN = "file:///variablenexport/miniCRAN_VariableMetadataPreparation"))
+.libPaths(c("./library", "z:/R/win-library-3.5",
+  "C:/Program Files/R/R-3.5.1/library"))
+
+
+
+option_list <- list(
+  optparse::make_option(c("-i", "--input-directory"),
+    type = "character",
+    action = "store", default = NA,
+    help = paste0(
+      "Path to the directory containing the input ",
+      "project directory with excel sheets, Json directory, and stata data."
+    ),
+    dest = "inputdirectory"
+  ),
+  optparse::make_option(c("-o", "--destination-directory"),
+    type = "character",
+    action = "store", default = NA,
+    help = paste0(
+      "Path to the directory where the work should",
+      "be stored after succesful runs."
+    ),
+    dest = "destinationdirectory"
+  )
+)
+
+option_parser <- optparse::OptionParser(option_list = option_list)
+
+copy_opts <- optparse::parse_args(option_parser)
+
+if (is.na(opt$inputdirectory)) {
+  optparse::print_help(option_parser)
+  stop("INPUT-DIRECTORY must not be empty!")
+}
+
+
+if (is.na(opt$destinationdirectory)) {
+  optparse::print_help(option_parser)
+  stop("DESTINATION-DIRECTORY must not be empty!")
+}
+
+
+copy_files_to_project_dir <- function(inputdirectory = copy_opts$inputdirectory,
+  destinationdirectory = copy_opts$destinationdirectory) {
+  file.copy(inputdirectory, destinationdirectory, recursive = TRUE)
+}
+
+
+copy_files_to_project_dir(opt$inputdirectory, opt$destinationdirectory)
