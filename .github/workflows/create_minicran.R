807c2dd9fa0106a2826f003c3e1fddf32012f711
René Reitmann <reitmann@dzhw.eu>
2021-03-15 12:12:06 +0100

    generate miniCRAN and bin artifacts (dzhw/metadatamanagement#2809)

--- .github/workflows/create_minicran.R
+++ .github/workflows/create_minicran.R
@@ -1,7 +1,7 @@
 #' Create miniCRAN instance including only Imports packages of the current package
 #' @param minicran_path The path where the miniCRAN instance is created
 #' @param r_version String containing the R version, e.g. "3.5"
-create_minicran <- function(minicran_path, r_version) {
+create_minicran <- function(minicran_path = "./miniCRAN", r_version = "3.5.1") {
   cran_server <- c(CRAN = "https://cran.rstudio.com")
   description <- desc::desc_get_deps(".")
   pkgs <- description$package[which(description$type=="Imports")]
@@ -19,5 +19,9 @@ create_minicran <- function(minicran_path, r_version) {
     type = c("source", "win.binary"),
     Rversion = r_version
   )
-
+  version <- desc::desc_get_field("Version", ".")
+  miniCRAN::addLocalPackage(paste0("variableMetadataPreparation_", version, ".tar.gz"),
+    pkgPath = ".", path = minicran_path)
 }
+
+create_minicran()
